3718888c7b38d5449e24527af9471fbbb466ef37
dirtyhands <e.frazin@gmail.com>
2018-01-22 22:05:37 -0800

    Create puzzle8.js

---
+++ puzzle8.js
@@ -0,0 +1,23 @@
+Function repeat(direction, max){
+Var count=0
+While (count < max) {
+		Direction
+		count=count+1
+}
+}
+
+Function downAndRight {
+Repeat (down,7)
+Repeat (right,2)
+}
+Function upAndRight {
+Repeat (up,7)
+Repeat (right,2)
+}
+
+Down
+downAndRight
+upAndRight
+downAndRight
+upAndRight
+downAndRight
