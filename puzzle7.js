1ba1cb436ca20d94afcc8d19645061dce6a64353
dirtyhands <e.frazin@gmail.com>
2018-01-22 22:05:13 -0800

    Create puzzle7.js

---
+++ puzzle7.js
@@ -0,0 +1,23 @@
+Function loopDown
+while(getColor!=”red”) {
+	Down
+}
+
+Function loopUp {
+While (getColor!=”red”) {
+	Up
+}
+}
+Function downAndUp {
+loopDown
+Right
+Right
+loopUp
+Right
+Right
+}
+
+downAndUp
+downAndUp
+loopDown
+Right
