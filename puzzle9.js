9de6415df25178db32de08cb3d36abb2d74f3a87
dirtyhands <e.frazin@gmail.com>
2018-01-25 10:10:27 -0800

    Update puzzle9.js

--- puzzle9.js
+++ puzzle9.js
@@ -1,4 +1,4 @@
-varGetColor = green 
+varTheColor=setColor
 Function repeat(direction, max){
 Var count=0
 While (count < max) {
@@ -6,7 +6,7 @@ While (count < max) {
 		count=count+1
 }
 }
-setColor(TheColor)
+setColor(green)
 Repeat (down,7)
 Repeat (right,4)
 Repeat (up,7)
