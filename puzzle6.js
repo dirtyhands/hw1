9c7d8b3215cbab5ed87cf415432a46604e3cfc77
dirtyhands <e.frazin@gmail.com>
2018-01-25 10:33:23 -0800

    Create puzzle6.js

---
+++ puzzle6.js
@@ -0,0 +1,22 @@
+Right
+var FirstColor = getColor
+if (getColor == "blue") {
+  down
+  down
+  down
+} else {
+  up
+  up
+  up
+  }
+}
+if (getColor == "blue"){
+  right
+} else {
+  left
+}
+if (firstColor == "blue") {
+  down
+} else {
+  up
+}
